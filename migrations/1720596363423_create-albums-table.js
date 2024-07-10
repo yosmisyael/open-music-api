7955560d6273beed5eb990b69f00a14840364dd1
yosev <yosmisyael7308@gmail.com>
2024-07-10 21:02:36 +0700

    feat: change table schema to use implicit timestamp

--- migrations/1720596363423_create-albums-table.js
+++ migrations/1720596363423_create-albums-table.js
@@ -21,14 +21,6 @@ exports.up = (pgm) => {
     year: {
       type: 'INTEGER',
       notNull: true
-    },
-    created_at: {
-      type: 'timestamp',
-      default: pgm.func('current_timestamp')
-    },
-    updated_at: {
-      type: 'timestamp',
-      default: pgm.func('current_timestamp')
     }
   })
 }
