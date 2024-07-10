4861230e375f709026a344ae55727a6a602527a0
yosev <yosmisyael7308@gmail.com>
2024-07-10 22:03:28 +0700

    feat: create albums service and save album method

---
+++ src/services/AlbumsService.js
@@ -0,0 +1,28 @@
+import pool from '../config/database.js'
+import { nanoid } from 'nanoid'
+import InvariantError from '../exceptions/InvariantError.js'
+
+class AlbumsService {
+  constructor () {
+    this._pool = pool
+  }
+
+  async addAlbum ({ title, year }) {
+    const id = `album-${nanoid(16)}`
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
+      values: [id, title, year]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Failed to add new album.')
+    }
+
+    return result.rows[0].id
+  }
+}
+
+export default AlbumsService
