15998a16dfe4479ca57e2743f7756c71a2b4b8a1
yosev <yosmisyael7308@gmail.com>
2024-07-11 17:06:24 +0700

    feat: remove unused validator

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,11 +1,8 @@
 import Joi from 'joi'
 
-export const AlbumPayloadSchema = Joi.object({
+const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
   year: Joi.number().required()
 })
 
-export const AlbumQuerySchema = Joi.object({
-  title: Joi.string().empty(),
-  performer: Joi.string().empty()
-})
+export default AlbumPayloadSchema
