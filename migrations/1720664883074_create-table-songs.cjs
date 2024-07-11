f5a13a80bf0e9ebbfaa9db356a5eef7eb27f0f8f
yosev <yosmisyael7308@gmail.com>
2024-07-11 13:47:15 +0700

    style: fix code style

--- migrations/1720664883074_create-table-songs.cjs
+++ migrations/1720664883074_create-table-songs.cjs
@@ -4,39 +4,39 @@
  * @returns {Promise<void> | void}
  */
 exports.up = (pgm) => {
-    pgm.createTable("songs", {
-        id: {
-            type: 'VARCHAR(25)',
-            primaryKey: true,
-        },
-        title: {
-            type: 'TEXT',
-            notNull: true
-        },
-        year: {
-            type: 'INTEGER',
-            notNull: true
-        },
-        genre: {
-            type: 'TEXT',
-            notNull: true
-        },
-        performer: {
-            type: 'TEXT',
-            notNull: true
-        },
-        duration: {
-            type: 'INTEGER',
-            allowNull: true
-        },
-        albumId: {
-            type: 'VARCHAR(25)',
-            allowNull: true,
-            references: 'albums',
-            onDelete: 'CASCADE'
-        }
-    })
-};
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(25)',
+      primaryKey: true
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true
+    },
+    duration: {
+      type: 'INTEGER',
+      allowNull: true
+    },
+    albumId: {
+      type: 'VARCHAR(25)',
+      allowNull: true,
+      references: 'albums',
+      onDelete: 'CASCADE'
+    }
+  })
+}
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
@@ -44,5 +44,5 @@ exports.up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
-    pgm.dropTable('songs')
-};
+  pgm.dropTable('songs')
+}
