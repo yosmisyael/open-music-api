250949e054b8a61e1c732f61585d873eb85c4aab
yosev <yosmisyael7308@gmail.com>
2024-07-11 11:22:17 +0700

    feat: create song validation

---
+++ src/validator/songs/index.js
@@ -0,0 +1,14 @@
+import SongPayloadSchema from './schema.js'
+import InvariantError from '../../exceptions/InvariantError.js'
+
+const SongValidator = {
+  validateSongPayload: (payload) => {
+    const validationResult = SongPayloadSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  }
+}
+
+export default SongValidator
---
+++ src/validator/songs/schema.js
@@ -0,0 +1,12 @@
+import Joi from 'joi'
+
+const SongPayloadSchema = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.number().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number().optional(),
+  albumId: Joi.string().optional()
+})
+
+export default SongPayloadSchema
