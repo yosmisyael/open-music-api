d9647f28dcc00104df06b8e8fa573e9288a48330
yosev <yosmisyael7308@gmail.com>
2024-07-11 12:37:55 +0700

    refactor: rename var for better consistency

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -5,7 +5,7 @@ export default {
   name: 'albums',
   version: '1.0.0',
   register: async (server, { service, validator }) => {
-    const albumHandler = new AlbumsHandler(service, validator)
-    server.route(routes(albumHandler))
+    const albumsHandler = new AlbumsHandler(service, validator)
+    server.route(routes(albumsHandler))
   }
 }
