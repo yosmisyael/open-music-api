864d0bf9ec9df2ee646c0286c1ca330131efb47d
yosev <yosmisyael7308@gmail.com>
2024-07-18 13:06:49 +0700

    feat: rename handler

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,11 +1,11 @@
-import UserHandler from './handler.js'
+import UsersHandler from './handler.js'
 import routes from './routes.js'
 
 export default {
   name: 'users',
   version: '1.0.0',
   register: async (server, { service, validator }) => {
-    const usersHandler = new UserHandler(service, validator)
+    const usersHandler = new UsersHandler(service, validator)
 
     server.route(routes(usersHandler))
   }
