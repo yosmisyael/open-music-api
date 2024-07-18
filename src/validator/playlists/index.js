ae93da9dc912f4486ead8676f2efb03635f47543
yosev <yosmisyael7308@gmail.com>
2024-07-18 23:59:46 +0700

    fix: fixed wrong schema usage

--- src/validator/playlists/index.js
+++ src/validator/playlists/index.js
@@ -1,10 +1,9 @@
-import { PostAuthenticationPayloadSchema } from '../authentications/schema.js'
 import InvariantError from '../../exceptions/InvariantError.js'
-import { PlaylistSongPayloadSchema } from './schema.js'
+import { PlaylistPayloadSchema, PlaylistSongPayloadSchema } from './schema.js'
 
 const PlaylistsValidator = {
   validatePostPlaylistsPayload: (payload) => {
-    const validationResult = PostAuthenticationPayloadSchema.validate(payload)
+    const validationResult = PlaylistPayloadSchema.validate(payload)
 
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message)
