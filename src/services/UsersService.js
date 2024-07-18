58769c44dccefaa7a30962aa0eca5d33f228a22d
yosev <yosmisyael7308@gmail.com>
2024-07-18 10:35:15 +0700

    feat: implement user service

---
+++ src/services/UsersService.js
@@ -0,0 +1,43 @@
+import pool from '../config/database.js'
+import InvariantError from '../exceptions/InvariantError.js'
+import { nanoid } from 'nanoid'
+import bcrypt from 'bcrypt'
+
+class UsersService {
+  constructor () {
+    this._pool = pool
+  }
+
+  async verifyNewUsername (username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Username already taken.')
+    }
+  }
+
+  async addUser ({ username, password, fullname }) {
+    await this.verifyNewUsername(username)
+
+    const id = `user-${nanoid(16)}`
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new InvariantError('Failed to register new user.')
+    }
+    return result.rows[0].id
+  }
+}
+
+export default UsersService
