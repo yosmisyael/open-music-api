7443951fb43f7b58ddf5c11171c2bf588752d834
yosev <yosmisyael7308@gmail.com>
2024-07-19 22:00:18 +0700

    feat: craete collaborations table

---
+++ migrations/1721400083238_create-table-collaborations.cjs
@@ -0,0 +1,36 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(25)',
+      primaryKey: true
+    },
+    playlist_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'CASCADE'
+    },
+    user_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'CASCADE'
+    }
+  })
+
+  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)')
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations')
+}
