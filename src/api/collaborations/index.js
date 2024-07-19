66fddbeb0a753e0472891883c383efb4924ffab2
yosev <yosmisyael7308@gmail.com>
2024-07-19 23:05:46 +0700

    feat: rename plugin name

--- src/api/collaborations/index.js
+++ src/api/collaborations/index.js
@@ -2,7 +2,7 @@ import CollaborationsHandler from './handler.js'
 import routes from './routes.js'
 
 export default {
-  name: 'users',
+  name: 'collaborations',
   version: '1.0.0',
   register: async (server, { collaborationsService, playlistsService, validator }) => {
     const collaborationsHandler = new CollaborationsHandler(collaborationsService, playlistsService, validator)
