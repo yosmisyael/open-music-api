687abcf384f7370ea99d15fefbed34ad6a7d5c33
yosev <yosmisyael7308@gmail.com>
2024-07-19 10:44:27 +0700

    feat: create playlist song pivot table migration

---
+++ migrations/1721320467960_create-table-playlist-songs.cjs
@@ -0,0 +1,36 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(25)',
+      primaryKey: true
+    },
+    playlist_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'CASCADE'
+    },
+    song_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'CASCADE'
+    }
+  })
+
+  pgm.addConstraint('playlists', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)')
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlists')
+}
