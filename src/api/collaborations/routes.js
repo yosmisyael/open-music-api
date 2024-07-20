eff5e0b6e58470e1ea3770848031fa262e577171
yosev <yosmisyael7308@gmail.com>
2024-07-20 18:53:48 +0700

    feat: add auth options

--- src/api/collaborations/routes.js
+++ src/api/collaborations/routes.js
@@ -2,12 +2,18 @@ const routes = (handler) => [
   {
     method: 'POST',
     path: '/collaborations',
-    handler: handler.postCollaborationHandler
+    handler: handler.postCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt'
+    }
   },
   {
     method: 'DELETE',
     path: '/collaborations',
-    handler: handler.deleteCollaborationHandler
+    handler: handler.deleteCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt'
+    }
   }
 ]
 
