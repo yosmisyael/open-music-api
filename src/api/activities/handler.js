1e1961c5581caf8f3a5013a51b6ced7c627dfc05
yosev <yosmisyael7308@gmail.com>
2024-07-20 20:43:47 +0700

    feat: implement get playlist activities api

--- src/api/activities/handler.js
+++ src/api/activities/handler.js
@@ -1,8 +1,8 @@
 import autoBind from 'auto-bind'
 
 class ActivitiesHandler {
-  constructor (activitiesServices) {
-    this._activiesServices = activitiesServices
+  constructor (service) {
+    this._activiesServices = service
 
     autoBind(this)
   }
@@ -10,7 +10,7 @@ class ActivitiesHandler {
   async getActivitiesHandler (request, h) {
     const { id } = request.params
 
-    const result = await this._activiesServices.getActivies(id)
+    const result = await this._activiesServices.getActivities(id)
 
     return h.response({
       status: 'success',
--- src/api/activities/index.js
+++ src/api/activities/index.js
@@ -4,8 +4,8 @@ import routes from './routes.js'
 export default {
   name: 'activities',
   version: '1.0.0',
-  register: (server, { activiesServices }) => {
-    const activitiesHandler = new ActivitiesHandler(activiesServices)
+  register: (server, { service }) => {
+    const activitiesHandler = new ActivitiesHandler(service)
 
     server.route(routes(activitiesHandler))
   }
