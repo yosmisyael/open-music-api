ec79b4eb5e39cd621b6c6ec82f53d567a0cdfc20
yosev <yosmisyael7308@gmail.com>
2024-07-23 22:33:38 +0700

    feat: create likes table migration

---
+++ migrations/1721748567556_create-table-likes.cjs
@@ -0,0 +1,30 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(25)',
+      primaryKey: true
+    },
+    user_id: {
+      type: 'VARCHAR(25)',
+      notNull: true
+    },
+    album_id: {
+      type: 'VARCHAR(25)',
+      notNull: true
+    }
+  })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('user_album_likes')
+}
