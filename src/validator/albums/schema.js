70fd1655bd04d53596ae40ee3154501f06aa0fd4
yosev <yosmisyael7308@gmail.com>
2024-07-23 21:26:19 +0700

    fix: fixed wrong property name that being validated

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -6,5 +6,5 @@ export const AlbumPayloadSchema = Joi.object({
 })
 
 export const AlbumCoverHeadersSchema = Joi.object({
-  cover: Joi.string().valid('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp').required()
+  'content-type': Joi.string().valid('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp').required()
 }).unknown()
