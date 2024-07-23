2d6ed76ffdf94c7d5c8d967e63a3fe3569728d9b
yosev <yosmisyael7308@gmail.com>
2024-07-23 12:27:41 +0700

    feat: create migration for column addition on albums

---
+++ migrations/1721712248648_add-cover-column-on-albums-table.cjs
@@ -0,0 +1,21 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumn('albums', {
+    cover: {
+      type: 'TEXT',
+      allowNull: true,
+      default: null
+    }
+  })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {}
