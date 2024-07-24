618b0a55190b17195913a8fbd5ac5079d1fba1c2
yosev <yosmisyael7308@gmail.com>
2024-07-24 09:34:10 +0700

    feat: add unique constraint

--- migrations/1721748567556_create-table-likes.cjs
+++ migrations/1721748567556_create-table-likes.cjs
@@ -11,13 +11,19 @@ exports.up = (pgm) => {
     },
     user_id: {
       type: 'VARCHAR(25)',
-      notNull: true
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE'
     },
     album_id: {
       type: 'VARCHAR(25)',
-      notNull: true
+      notNull: true,
+      references: 'albums',
+      onDelete: 'CASCADE'
     }
   })
+
+  pgm.addConstraint('user_album_likes', 'unique_user_id_and_album_id', 'UNIQUE(user_id, album_id)')
 }
 
 /**
